8565255507c280d4877394fb921e081a1a75bb66
Tapur <113691557+shuamamine@users.noreply.github.com>
2023-03-16 19:42:48 +0530

    Create pattern edcba dcba cba ba a.c

---
+++ programs/pattern edcba dcba cba ba a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int i,j; 
+    for(i = 69; i>=65;i-- )
+    {
+      for(j=i;j>=65;j--)
+      {
+          printf("%c", j);
+      }
+      printf("\n");
+    }
+  return 0;
+}
