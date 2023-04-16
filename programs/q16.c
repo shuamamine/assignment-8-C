20ac8e7027af7bd6632265010c0eac028eff10b5
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-16 23:03:21 +0530

    Update q16.c

--- programs/q16.c
+++ programs/q16.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+{
+    int c=1;
+    for(int i=5;i>=1;i--){
+        int num=65;
+        printf("A");
+        for(int j=i-1;j>=1;j--){
+            printf("%c",num+c);
+            num=num+c;
+        }
+        printf("\n");
+        count++;
+    }
+    return 0;
+}
