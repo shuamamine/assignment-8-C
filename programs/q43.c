8a5daccc3cb11f84765f2ebddfce2cebf22b3912
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-16 23:00:48 +0530

    Update q43.c

--- programs/q43.c
+++ programs/q43.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main()
+{
+  int l = 8,l1 = 5,in=0;
+  for(int i = 0; i <=3;i++)
+  {
+    printf("abcd")
+      for(int j = 0; j < l;j++)
+      {
+        printf("%c", 'A'+i);
+      }  
+      for(int j = 0; j < l1-in;j++)
+      {
+        printf("%d", j+incre);
+      }  
+      l++;
+      incre++;
+    printf("\n");
+  }
+  return 0;
+}
